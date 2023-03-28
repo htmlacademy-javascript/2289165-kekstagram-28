9941a40b4f5389c216d90721475a5070c15da34c
Ekaterina Semenets <cortes.lex@gmail.com>
2023-03-28 18:41:29 +0300

    fix: get photo specification from create-objects

--- js/create-pictures.js
+++ js/create-pictures.js
@@ -1,30 +1,28 @@
-import {SIMILAR_PHOTO_SPECIFICATION_COUNT} from './data.js';
-import {createPhotoSpecification} from './create-objects.js';
+import { SIMILAR_PHOTO_SPECIFICATION_COUNT } from './data.js';
+import {similarPhotoSpecifications } from './create-objects.js';
 
 const container = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const pictures = [];
-const fragment = document.createDocumentFragment();
 
-function createMiniature({url, likes, comments}) {
+
+function createMiniature({ url, likes, comments }) {
   const newElement = pictureTemplate.cloneNode(true);
   newElement.querySelector('.picture__img').src = url;
-  newElement.querySelector('.picture__likes').textContent = likes ;
+  newElement.querySelector('.picture__likes').textContent = likes;
   newElement.querySelector('.picture__comments').textContent = comments.length;
 
   return newElement;
-}
-
-for (let i = 0; i < SIMILAR_PHOTO_SPECIFICATION_COUNT; i++) {
-  pictures[i] = createMiniature(createPhotoSpecification());
-  fragment.appendChild(pictures[i]);
 }
 
 /**
  * Create miniphotos on the main page
  */
 function createModels() {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < SIMILAR_PHOTO_SPECIFICATION_COUNT; i++) {
+    fragment.appendChild(createMiniature(similarPhotoSpecifications[i]));
+  }
   container.appendChild(fragment);
 }
 
-export {createModels};
+export { createModels };
