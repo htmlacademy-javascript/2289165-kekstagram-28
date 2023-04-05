8a9d1e6d639e80831d341bdc3930fbf5b60a8ec6
Ekaterina Semenets <cortes.lex@gmail.com>
2023-04-05 11:50:27 +0300

    fix: fix problem with commets, change arguments and some variables, add utils.js for similar functions for different modules.

--- js/full-size-picture.js
+++ js/full-size-picture.js
@@ -1,7 +1,7 @@
 import { similarPhotoSpecifications } from './create-objects.js';
 import { container } from './create-pictures.js';
-
-const MAXIMUM_COMMENTS_NUMBER = 5;
+import { MAXIMUM_COMMENTS_NUMBER } from './data.js';
+import { isEscapeKey } from './utils.js';
 
 const fullSize = document.querySelector('.big-picture');
 const fullSizePicture = fullSize.querySelector('.big-picture__img img');
@@ -9,8 +9,8 @@ const likesCount = fullSize.querySelector('.likes-count');
 const commentsCount = fullSize.querySelector('.comments-count');
 const commentsList = fullSize.querySelector('.social__comments');
 const textAboutPicture = fullSize.querySelector('.social__caption');
-const commentsCountBlock = document.querySelector('.social__comment-count');
-const commentsLoader = document.querySelector('.comments-loader');
+const commentsCountBlock = fullSize.querySelector('.social__comment-count'); //!
+const commentsLoader = fullSize.querySelector('.comments-loader');//!
 const closeButton = fullSize.querySelector('.big-picture__cancel');
 const oneCommentType = commentsList.querySelector(`li:nth-child(${1})`).cloneNode(true);
 const loadMoreCommentsButton = fullSize.querySelector('.comments-loader');
@@ -18,15 +18,15 @@ const loadMoreCommentsButton = fullSize.querySelector('.comments-loader');
 
 container.addEventListener('click', onContainerClick);
 closeButton.addEventListener('click', onCloseButtonClick);
+loadMoreCommentsButton.addEventListener('click', () => onLoadMoreCommentsButtonClick(getDataForPicture(loadMoreCommentsButton.id).comments));
 
 function onContainerClick(evt) {
-  if (evt.target.className === 'picture__img') {
-    enlargeMiniature(getDataForPicture(evt.target.closest('a').id));
-  }
+  enlargeMiniature(getDataForPicture(evt.target.closest('a').id));
+
 }
 
 function onFullSizeEscKeydown(evt) {
-  if (evt.key === 'Escape') {
+  if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeFullSize();
   }
@@ -36,8 +36,8 @@ function onCloseButtonClick() {
   closeFullSize();
 }
 
-function onLoadMoreCommentsButtonClick({ comments }) {
-  commentsList.appendChild(loadMoreComments({ comments }));
+function onLoadMoreCommentsButtonClick(comments) {
+  commentsList.appendChild(loadMoreComments(comments));
   commentsCountBlock.innerHTML = `${commentsList.childElementCount} из ${comments.length} комментариев`;
 }
 
@@ -51,41 +51,38 @@ function enlargeMiniature({ id, url, likes, comments, description }) {
   fullSizePicture.src = url;
   likesCount.textContent = likes;
   commentsCount.textContent = comments.length;
+  loadMoreCommentsButton.id = fullSizePicture.id;
 
   commentsList.replaceChildren();
-  const dataOfPicture = getDataForPicture(fullSizePicture.id);
   for (let i = 0; i < MAXIMUM_COMMENTS_NUMBER; i++) {
-    commentsList.appendChild(createOneComment(dataOfPicture, i));
+    commentsList.appendChild(createOneComment(getDataForPicture(fullSizePicture.id).comments, i));
   }
   textAboutPicture.textContent = description;
 
   commentsCountBlock.innerHTML = `${commentsList.childElementCount} из ${comments.length} комментариев`;
   commentsCountBlock.classList.remove('hidden');
   commentsLoader.classList.remove('hidden');
-
-  loadMoreCommentsButton.addEventListener('click', () => onLoadMoreCommentsButtonClick(dataOfPicture));
 }
 
 function getDataForPicture(id) {
-  const data = similarPhotoSpecifications
+  return similarPhotoSpecifications
     .find((photoSpecification) => Number(id) === photoSpecification.id);
-  return data;
 }
 
-function createOneComment({ comments }, count) {
+function createOneComment(comments, index) {
   const oneComment = oneCommentType.cloneNode(true);
-  oneComment.querySelector('.social__text').textContent = comments[count].message;
-  oneComment.querySelector('.social__picture').src = comments[count].avatar;
-  oneComment.querySelector('.social__picture').alt = comments[count].name;
+  oneComment.querySelector('.social__text').textContent = comments[index].message;
+  oneComment.querySelector('.social__picture').src = comments[index].avatar;
+  oneComment.querySelector('.social__picture').alt = comments[index].name;
   return oneComment;
 }
 
-function loadMoreComments({ comments }) {
+function loadMoreComments(comments) {
   const fragment = document.createDocumentFragment();
-  for (let i = commentsList.childElementCount;
-    i < Math.min(comments.length, commentsList.childElementCount + MAXIMUM_COMMENTS_NUMBER);
+  for (let i = commentsList.children.length /*commentsList.childElementCount*/;
+    i < Math.min(comments.length, commentsList.children.length + MAXIMUM_COMMENTS_NUMBER);
     i++) {
-    fragment.appendChild(createOneComment({ comments }, i));
+    fragment.appendChild(createOneComment(comments, i));
 
   }
   return fragment;
